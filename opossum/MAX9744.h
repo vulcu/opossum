b0287fb030c0ffe1c11ad5a7367adc87d1685e17
Winry R. Litwa-Vulcu <19890187+vulcu@users.noreply.github.com>
2021-03-21 23:30:08 -0400

    added in probably all the methods needed, although driver not presently working

--- opossum/MAX9744.h
+++ opossum/MAX9744.h
@@ -16,49 +16,123 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-class MAX9744 {
-  private:
-    #include <avr/pgmspace.h>
+#ifndef __MAX9744_H__
+#define __MAX9744_H__
 
-    #include "parameters.h"
+  #include <avr/pgmspace.h>
+  #include <Wire.h>
 
-    // MAX9744 amplifier gain levels (dB), multiplied
-    // by 10x to allow PROGMEM storage as int16_t
-    const int16_t MAX9744GainLevel[64] PROGMEM = 
-    {
-        95,    88,    82,    76,
-        70,    65,    59,    54,
-        49,    44,    39,    34,
-        29,    24,    20,    16,
-        12,    05,   -05,   -19,
-        -34,   -50,   -60,   -71,
-        -89,   -99,  -109,  -120,
-        -131,  -144,  -154,  -164,
-        -175,  -197,  -216,  -235,
-        -252,  -272,  -298,  -315,
-        -334,  -360,  -376,  -396,
-        -421,  -437,  -456,  -481,
-        -506,  -542,  -567,  -602,
-        -627,  -662,  -687,  -722,
-        -747,  -783,  -808,  -843,
-        -868,  -903,  -929, -1095
-    };
+  class MAX9744 {
+    public:
+      MAX9744(uint8_t i2c_addr, uint8_t mute_pin, uint8_t shutdown_pin, bool init_twi) {
+        // Use 'this->' to make the difference between the 'pin' 
+        // attribute of the class and the local variable
+        this->i2c_addr = i2c_addr;
+        this->mute_pin = mute_pin;
+        this->shutdown_pin = shutdown_pin;
+        this->init_twi = init_twi;
+      }
+
+      void init(void) {
+        // initialize the MAX9744 mute and shutdown signals
+        pinMode(mute_pin, OUTPUT);
+        pinMode(shutdown_pin, OUTPUT);
+
+        // mute the MAX9744 then take it out of shutdown
+        mute();
+        enable();
+
+        if (init_twi) {
+          // do this check so that Wire only gets initialized once
+          //if (TWCR ^ (0x01 << TWEN)) {
+            // if TWI not enabled, initialize Wire library
+            Wire.begin();
+          //}
+          // set TWI clock rate (default is 400000L)
+          Wire.setClock(TWI_CLOCK_RATE);
+        }
+      }
+
+      void enable(void) {
+        // enable the MAX9744 by taking it out of shutdown (HIGH)
+        digitalWrite(shutdown_pin, (HIGH));
+      }
+
+      void invertMuteLogic(bool invert_mute) {
+        // invert the mute signal (needed for use with some MAX9744 kits)
+        this->invert_mute = invert_mute;
+      }
+
+      void mute(void) {
+        // mute the MAX9744 output (LOW)
+        if (invert_mute) {
+          digitalWrite(shutdown_pin, HIGH);
+        }
+        else {
+          digitalWrite(shutdown_pin, LOW);
+        }
+      }
+
+      void shutdown(void) {
+        // disable the MAX9744 by putting it into shutdown (LOW)
+        digitalWrite(shutdown_pin, LOW);
+      }
+
+      void unmute(void) {
+        // unmute the MAX9744 output (HIGH)
+        if (invert_mute) {
+          digitalWrite(shutdown_pin, LOW);
+        }
+        else {
+          digitalWrite(shutdown_pin, HIGH);
+        }
+      }
+
+      void volume(uint8_t value) {
+        // initialize the MSGEQ7 reset and strobe signals
+        // & (TWCR & (0x01 << TWEN)))
+        if (value < 64) {
+          Wire.beginTransmission(i2c_addr);
+            Wire.write(value);
+          Wire.endTransmission();
+        }
+      }
 
 
-  public:
-    MAX9744(void) {
-      // create driver object
-    }
+    private:
+      #ifndef TWI_CLOCK_RATE
+        #define TWI_CLOCK_RATE (int32_t)400000
+      #endif
 
-    
-    void init(void) {
-      // initialize the MSGEQ7 reset and strobe signals
-      pinMode(STROBE, OUTPUT);
-    }
+      bool init_twi;
+      bool invert_mute = false;
+      uint8_t i2c_addr;
+      uint8_t mute_pin;
+      uint8_t shutdown_pin;
 
+      // MAX9744 amplifier gain levels (dB), multiplied
+      // by 10x to allow PROGMEM storage as int16_t
+      /*
+      const int16_t MAX9744GainLevel[64] PROGMEM = 
+        {
+            95,    88,    82,    76,
+            70,    65,    59,    54,
+            49,    44,    39,    34,
+            29,    24,    20,    16,
+            12,    05,   -05,   -19,
+            -34,   -50,   -60,   -71,
+            -89,   -99,  -109,  -120,
+            -131,  -144,  -154,  -164,
+            -175,  -197,  -216,  -235,
+            -252,  -272,  -298,  -315,
+            -334,  -360,  -376,  -396,
+            -421,  -437,  -456,  -481,
+            -506,  -542,  -567,  -602,
+            -627,  -662,  -687,  -722,
+            -747,  -783,  -808,  -843,
+            -868,  -903,  -929, -1095
+        };
+        */
+  };
 
-    void mute(void) {
-      // initialize the MSGEQ7 reset and strobe signals
-      pinMode(STROBE, OUTPUT);
-    }
-};
+#endif
