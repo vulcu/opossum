b0d95a9c9b6f3fea7f18d58c65892cb65a564780
Winry R. Litwa-Vulcu <19890187+vulcu@users.noreply.github.com>
2021-04-11 16:27:57 -0400

    rename 'math.h' to 'audiomath.h'
    * move `dBFastRelativeLevel` to 'audiomath.h'
    * rename `dBLevs` to `dBLevels`

--- Opossum.ino
+++ Opossum.ino
@@ -24,6 +24,8 @@
 #include "opossum/ledbutton.cpp"
 #include "opossum/BM62.h"
 #include "opossum/BM62.cpp"
+#include "opossum/audiomath.h"
+#include "opossum/audiomath.cpp"
 #include "opossum/MAX9744.h"
 #include "opossum/MAX9744.cpp"
 #include "opossum/MSGEQ7.h"
@@ -31,16 +33,6 @@
 
 // comment to deactivate UART debug mode
 #define DEBUG
-
-// Coefficient table for fast dB approximations
-const uint16_t dBCoefTable[25] PROGMEM =
-{ 
-  2053, 2175, 2303, 2440, 2584,
-  2738, 2900, 3072, 3254, 3446,
-  3651, 3867, 4096, 4339, 4596,
-  4868, 5157, 5462, 5786, 6129,
-  6492, 6876, 7284, 7715, 8173
-};
 
 // buffer index, volume, filtered volume, base level, present level
 uint8_t  bufferIndx = 0;
@@ -51,7 +43,7 @@ uint16_t zeroSignal, baseLevel, levelOut = 251;
 // audio levels, audio level buffer, and approx. relative dB levels
 uint16_t levelRead[7];
 uint16_t levelBuf[32];
-uint16_t dBLevs[25];
+uint16_t dBLevels[25];
 
 // time of most recent audio level read (rolls over after about 50 days)
 uint32_t previousMillis = 0;
@@ -209,14 +201,6 @@ uint16_t expDecayBuf(uint16_t levelReadMean) {
 }
 
 
-// update the relative dB level bands using currently defined volume level
-void dBFastRelativeLevel(void) {
-  for(uint8_t k = 0; k < 25; k++) {
-    dBLevs[k] = ((uint32_t)baseLevel * pgm_read_word(&(dBCoefTable[k]))) >> 12;
-  }
-}
-
-
 // calculate allowable MAX9744 volume adjustment range
 void updateVolumeRange(void) {
   uint8_t CurrentVolumeLevel = lowByte(volOut >> 4);
@@ -355,7 +339,7 @@ void loop() {
         Serial.print(" ");
       }
       for(uint8_t k = 0; k < 25; k++) {
-        Serial.print(dBLevs[k]);
+        Serial.print(dBLevels[k]);
         Serial.print(" ");
       }
       Serial.print("\n");
---
+++ opossum/audiomath.h
@@ -0,0 +1,29 @@
+/*
+ * audiomath.h - Math library for Opossum BT Aplifier
+ * Copyright (c) 2017 Winry R. Litwa-Vulcu. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __OPOSSUM_AUDIO_MATH_H__
+#define __OPOSSUM_AUDIO_MATH_H__
+
+  class Audiomath {
+    public:
+      static void dBFastRelativeLevel(uint16_t *dBLevels, uint16_t baseLevel);
+      //uint16_t power_uint8(uint8_t base, uint8_t exponent);
+      //int16_t quantizeValueToNearest(int16_t value, uint8_t quantizeStep);
+  };
+
+#endif
